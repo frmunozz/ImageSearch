f1c6224cec9b16fe82c9cc432a7a9d3eb8dd9953
pancho <skay123>
2019-07-07 00:51:45 -0400

    agrego regressor arreglado

--- src/regressor.py
+++ src/regressor.py
@@ -1,11 +1,14 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import model_from_json
+from keras.callbacks import EarlyStopping
 from sklearn.utils import shuffle as shfle
 from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler
 import numpy as np
 import matplotlib.pyplot as plt
 from .validator import validate_keys
+import json
+from keras.models import load_model
 
 
 _VALID_KWARGS = {"input_dim": None, 
@@ -17,6 +20,8 @@ _VALID_KWARGS = {"input_dim": None,
                  "optimizer": "adam",
                  "metrics": ["mae", "accuracy"],
                  "model": None,
+                 "history": None,
+                 "early-stopping": EarlyStopping(monitor="val_loss", patience=4, verbose=True, mode="min", min_delta=0.001),
                  "scaler_input": None,
                  "scaler_output": None,
                  "epochs": 10,
@@ -24,6 +29,24 @@ _VALID_KWARGS = {"input_dim": None,
                  "validation_split": 0.2,
                  "shuffle": True,
                  }
+
+# coefficient of determination (R^2) for regression  (only for Keras tensors)
+def r_square(y_true, y_pred):
+    from keras import backend as K
+    SS_res =  K.sum(K.square(y_true - y_pred)) 
+    SS_tot = K.sum(K.square(y_true - K.mean(y_true))) 
+    return ( 1 - SS_res/(SS_tot + K.epsilon()) )
+
+# root mean squared error (rmse) for regression (only for Keras tensors)
+def rmse(y_true, y_pred):
+    from keras import backend
+    return backend.sqrt(backend.mean(backend.square(y_pred - y_true), axis=-1))
+
+# mean squared error (mse) for regression  (only for Keras tensors)
+def mse(y_true, y_pred):
+    from keras import backend
+    return backend.mean(backend.square(y_pred - y_true), axis=-1)
+
 
 class Regressor(object):
     
@@ -66,32 +89,33 @@ class Regressor(object):
             self.kwargs["scaler_output"].fit(y_train)
             y_train = self.kwargs["scaler_output"].transform(y_train)
 
-        results=self.kwargs["model"].fit(X_train,y_train, epochs=self["epochs"], 
+        history=self.kwargs["model"].fit(X_train,y_train, epochs=self["epochs"], 
                                          validation_split=self["validation_split"],shuffle=self["shuffle"],
-                                         batch_size=self["batch_size"])
-        return results
+                                         batch_size=self["batch_size"], callbacks=[self.kwargs['early-stopping']])
+        self.kwargs["history"] = history.history
+        return history
     
-    def save(self, model_file="model"):
-        # serialize model to JSON
-        model_json = self.kwargs["model"].to_json()
-        with open("./model/" + model_file + ".json", "w") as json_file:
-            json_file.write(model_json)
-        # serialize weights to HDF5
-        self.kwargs["model"].save_weights("./model/" + model_file + ".h5")
-        print("Saved model to disk")
+    def save(self, name):
+        print("saving model to disk ...", end="")
+        # save model to hdf5
+        self.kwargs["model"].save("./model/" + name + ".h5")
+
+        # save history
+        json.dump(self.kwargs["history"], open("./model/" + name + "_history.json", "w"))
+        print(" done!")
     
         
-    def load(self, model_file="model"):
-        # load json and create model
-        json_file = open("./model/" + model_file + '.json', 'r')
-        loaded_model_json = json_file.read()
-        json_file.close()
-        model = model_from_json(loaded_model_json)
-        # load weights into new model
-        model.load_weights("./model/" + model_file + ".h5")
-        print("Loaded model from disk")
-        model.compile(loss=self["loss"], optimizer=self["optimizer"], metrics=self["metrics"])
-        self.kwargs["model"] = model
+    def load(self, name, custom_objects=None):
+        print("loading model from disk ...", end="")
+        self.kwargs["model"] = load_model("./model/" + name + ".h5", custom_objects=custom_objects)
+        # load history
+        self.kwargs["history"] = json.load(open("./model/" + name + "_history.json", 'r'))
+        print(" done!")
+
+        # print("Loaded model from disk")
+        # model.compile(loss=self["loss"], optimizer=self["optimizer"], metrics=self["metrics"])
+        # self.kwargs["model"] = model
+        return self.kwargs["history"]
     
     def evaluate(self, X_test, y_test):
         print("evaluando test")
@@ -99,9 +123,9 @@ class Regressor(object):
             X_test = self.kwargs["scaler_input"].transform(X_test)
             y_test = self.kwargs["scaler_output"].transform(y_test)
         score = self.kwargs["model"].evaluate(X_test, y_test, verbose=True)
-        print("%s: %.2f" % (self.kwargs["model"].metrics_names[1], score[1]))
-        print("%s: %.2f" % (self.kwargs["model"].metrics_names[0], score[0]))
-        print("%s: %.2f%%" % (self.kwargs["model"].metrics_names[2], score[2]*100))
+        print("    EVALUATION RESULTS :  ")
+        for i in range(len(score)):
+            print("%s: %.2f" % (self.kwargs["model"].metrics_names[i], score[i]))
         return score
         
     def get_model(self):
@@ -124,28 +148,45 @@ class Regressor(object):
             X_reversed = self.kwargs["scaler_input"].inverse_transform(X)
             assert X == X_reversed
         return y_pred
-    
-def plot_metrics(history):
-    print(history.history.keys())
-    fig, ax = plt.subplots(1, 3, figsize=(16, 4))
-    ax[0].plot(history.history['loss'])
-    ax[0].plot(history.history['val_loss'])
-    ax[0].set_title('model loss')
-    ax[0].set_ylabel('loss')
-    ax[0].set_xlabel('epoch')
-    ax[0].legend(['train', 'validation'], loc='upper left')
 
-    ax[1].plot(history.history['acc'])
-    ax[1].plot(history.history['val_acc'])
-    ax[1].set_title('model acc')
-    ax[1].set_ylabel('acc')
-    ax[1].set_xlabel('epoch')
-    ax[1].legend(['train', 'validation'], loc='upper left')
+    def plot_metrics(self, main_title="metricas", name=None):
+        metrics = self["model"].__dict__["metrics"]
+        fig, ax = plt.subplots(1, len(metrics), figsize=(16, 4))
+        for i, k in enumerate(metrics):
+            if not isinstance(k, str):
+                k = k.__name__
+            elif k == "mae":
+                k = "mean_absolute_error"
+            ax[i].plot(self['history'][k])
+            ax[i].plot(self['history']['val_' + k])
+            ax[i].set_title('model ' + k)
+            ax[i].set_ylabel(k)
+            ax[i].set_xlabel('epoch')
+            ax[i].legend(['train', 'validation'], loc='upper left')
 
-    ax[2].plot(history.history['mean_absolute_error'])
-    ax[2].plot(history.history['val_mean_absolute_error'])
-    ax[2].set_title('model MSE')
-    ax[2].set_ylabel('MSE')
-    ax[2].set_xlabel('epoch')
-    ax[2].legend(['train', 'validation'], loc='upper left')
+        fig.suptitle(main_title)
+        plt.tight_layout()
+        fig.subplots_adjust(top=0.88)
+        if name is not None:
+            plt.savefig("./figures/" + name + ".png", dpi=300)
+
+        return fig, ax
+
+
+
+def plot_metrics(history, metrics_keys, name="model"):
+    metrics = self["model"].__dict__["kwargs"]["metrics"]
+
+    fig, ax = plt.subplots(1, len(metrics_keys), figsize=(16, 4))
+    for i, k in enumerate(metrics_keys):
+        ax[i].plot(history[k])
+        ax[i].plot(history['val_' + k])
+        ax[i].set_title('model ' + k)
+        ax[i].set_ylabel(k)
+        ax[i].set_xlabel('epoch')
+        ax[i].legend(['train', 'validation'], loc='upper left')
+
+    plt.tight_layout()
+    plt.savefig("./figures/" + name + ".png", dpi=300)
+
     return fig, ax
